43425e298db06495c5f6097ee72572a0228ff6d7
Hussain <Majid.Hussain@lubrizol.com>
2017-03-25 14:48:01 +0000

    Implemented bubblesort

---
+++ bubblesort.js
@@ -0,0 +1,33 @@
+//1st implementation
+// _
+//[3,2,1] 3 > 2 if so swap
+//   _
+//[2,3,1] 3 > 1 if so swap
+// _
+//[2,1,3] 2 > 1 if so swap
+//   _
+//[1,2,3] 2 > 1 
+//Scanning will happen array.length-1 time
+
+function bubblesort(unsortedItems) {
+    for (var i = 0; i < unsortedItems.length; i++) {
+        for (var j = 0; j < unsortedItems.length - 1 - i; j++) //unsortedItems.length - 1 - i stop sorting already sorted items.
+            var firstItem = unsortedItems[j];
+        var secondItem = unsortedItems[j + 1];
+        if (firstItem > secondItem) {
+            unsortedItems = swap(firstItem, secondItem, unsortedItems);
+        }
+    }
+}
+return unsortedItems;
+}
+
+function swap(firstItem, secondItem, unsortedItems) {
+    var firstItemIndex = unsortedItems.indexOf(firstItem);
+    var secondItemIndex = unsortedItems.indexOf(secondItem);
+    unsortedItems[firstItemIndex] = secondItem;
+    unsortedItems[secondItemIndex] = firstItem;
+    return unsortedItems;
+}
+
+console.log(bubblesort([1, 2, 0, 4, 5, -10]));
